4a1d0576ba3375f3f8937e5ce474281981976629
Ivan Perez <ivan.pmartell@hotmail.com>
2022-11-13 22:05:50 -0800

    Minor setup changes

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 apt-get -y update && apt-get install -y wget build-essential zlib1g zlib1g-dev python3-pip ttf-mscorefonts-installer
 
 wget https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.2-linux-x86_64.tar.gz && \
-    tar xvf julia-1.8.2-linux-x86_64.tar.gz
+    tar xvf julia-1.8.2-linux-x86_64.tar.gz && \
+    rm -f julia-1.8.2-linux-x86_64.tar.gz
 
 wget https://github.com/weizhongli/cdhit/releases/download/V4.8.1/cd-hit-v4.8.1-2019-0228.tar.gz && \
     tar xvf cd-hit-v4.8.1-2019-0228.tar.gz && \
-    rm -f cd-hit-v4.8.1-2019-0228.tar.gz \
+    rm -f cd-hit-v4.8.1-2019-0228.tar.gz && \
     mv cd-hit-v4.8.1-2019-0228 cd-hit && \
     cd cd-hit && \
     make && \
@@ -30,6 +31,8 @@ wget wget ftp://emboss.open-bio.org/pub/EMBOSS/emboss-latest.tar.gz && \
     make install && \
     cd ..
 
-echo "Add the following folders to PATH: cd-hit/  cd-hit/cd-hit-auxtools  cd-hit/psi-cd-hit  ncbi-blast-2.8.1+/bin julia-1.8.2/bin"
-echo "Replace pdb-sam with full path to repository folder: PATH=pdb-sam/julia-1.8.2/bin:pdb-sam/cd-hit:pdb-sam/cd-hit/cd-hit-auxtools:pdb-sam/cd-hit/psi-cd-hit:pdb-sam/ncbi-blast-2.8.1+/bin:/usr/local/emboss/bin:${PATH}"
-echo "Add julia and python libraries in README.md"
+echo "Add the following folders to PATH inside the ~/.profile file: cd-hit/  cd-hit/cd-hit-auxtools  cd-hit/psi-cd-hit  ncbi-blast-2.8.1+/bin julia-1.8.2/bin"
+echo "Replace pdb-sam with full path to repository folder: PATH=\"pdb-sam/julia-1.8.2/bin:pdb-sam/cd-hit:pdb-sam/cd-hit/cd-hit-auxtools:pdb-sam/cd-hit/psi-cd-hit:pdb-sam/ncbi-blast-2.8.1+/bin:/usr/local/emboss/bin:\$PATH\""
+echo "Add julia and python libraries from README.md"
+
+echo "PATH=\"$(pwd)/julia-1.8.2/bin:$(pwd)/cd-hit:$(pwd)/cd-hit/cd-hit-auxtools:$(pwd)/cd-hit/psi-cd-hit:$(pwd)/ncbi-blast-2.8.1+/bin:/usr/local/emboss/bin:\$PATH\"" >> ~/.profile
