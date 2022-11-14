0118aef5b7e9d0aca85ee5f6d38615ac9208b73a
Ubuntu <ivan@Ivan-VM.zob0ykevfdnenmtb4mdjqqvwxe.xx.internal.cloudapp.net>
2022-11-14 06:23:51 +0000

    Added bashrc PATH to setup

--- setup.sh
+++ setup.sh
@@ -31,8 +31,9 @@ wget wget ftp://emboss.open-bio.org/pub/EMBOSS/emboss-latest.tar.gz && \
     make install && \
     cd ..
 
-echo "Add the following folders to PATH inside the ~/.profile file: cd-hit/  cd-hit/cd-hit-auxtools  cd-hit/psi-cd-hit  ncbi-blast-2.8.1+/bin julia-1.8.2/bin"
+echo "Add the following folders to PATH inside the ~/.profile and ~/.bashrc file: cd-hit/  cd-hit/cd-hit-auxtools  cd-hit/psi-cd-hit  ncbi-blast-2.8.1+/bin julia-1.8.2/bin"
 echo "Replace pdb-sam with full path to repository folder: PATH=\"pdb-sam/julia-1.8.2/bin:pdb-sam/cd-hit:pdb-sam/cd-hit/cd-hit-auxtools:pdb-sam/cd-hit/psi-cd-hit:pdb-sam/ncbi-blast-2.8.1+/bin:/usr/local/emboss/bin:\$PATH\""
 echo "Add julia and python libraries from README.md"
 
 echo "PATH=\"$(pwd)/julia-1.8.2/bin:$(pwd)/cd-hit:$(pwd)/cd-hit/cd-hit-auxtools:$(pwd)/cd-hit/psi-cd-hit:$(pwd)/ncbi-blast-2.8.1+/bin:/usr/local/emboss/bin:\$PATH\"" >> ~/.profile
+echo "PATH=\"$(pwd)/julia-1.8.2/bin:$(pwd)/cd-hit:$(pwd)/cd-hit/cd-hit-auxtools:$(pwd)/cd-hit/psi-cd-hit:$(pwd)/ncbi-blast-2.8.1+/bin:/usr/local/emboss/bin:\$PATH\"" >> ~/.bashrc
